e51138ac2f408d65a716d112c4d14606aa06925e
Thaocute02 <133091219+Thaocute02@users.noreply.github.com>
2023-07-19 15:47:58 +0700

    Update delete.php

--- product/delete.php
+++ product/delete.php
@@ -3,5 +3,5 @@
     $product = new Product(null, null, null);
     $id = ['id' => $_POST['id']];
     $product->deleteProdData($id);
-    header("Location: http://localhost/learn_php/BTVN11/product/index.php");
+    header("Location: http://localhost:8080/bai11/product/index.php");
 ?>
